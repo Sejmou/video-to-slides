b9bb8199fccc9b323e0398a6da04663539bbc2bd
Sejmou <samo.kolter@gmail.com>
2022-06-29 22:06:19 +0200

    fix extremely large output PDF page size

--- src/content/util/js-pdf.ts
+++ src/content/util/js-pdf.ts
@@ -13,7 +13,11 @@ export async function addImageToJsPdf(
   doc: jsPDF,
   ocrText?: string
 ) {
-  doc.addPage([VIDEOHEIGHT, VIDEOWIDTH], 'landscape');
+  const scaleFactor = 297 / VIDEOWIDTH; // PDF pages use mm as unit per default; an A4 format landscape page is 297 mm wide -> scale down images to that size (in mm)
+  const pageWidth = VIDEOWIDTH * scaleFactor;
+  const pageHeight = VIDEOHEIGHT * scaleFactor;
+
+  doc.addPage([pageHeight, pageWidth], 'landscape');
   const imgDataUrl = await fileToDataUrl(imgFile);
   const img = document.createElement('img');
   img.src = imgDataUrl;
@@ -21,15 +25,15 @@ export async function addImageToJsPdf(
     imageData: img,
     x: 0,
     y: 0,
-    width: VIDEOWIDTH,
-    height: VIDEOHEIGHT,
+    width: pageWidth,
+    height: pageHeight,
     compression: 'SLOW',
   });
   if (ocrText) {
     doc.text(
       ocrText,
-      VIDEOWIDTH / 2,
-      VIDEOHEIGHT / 2, // place text roughly in the middle to make text search more convenient (should focus into center of slide where text was found)
+      pageWidth / 2,
+      pageHeight / 2, // place text roughly in the middle to make text search more convenient (should focus into center of slide where text was found)
       {
         renderingMode: 'invisible',
       }
